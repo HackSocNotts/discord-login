b6af52981493a976c69bb06532370c86eadaf28d
Aaron Osher <git@aaronosher.io>
2020-11-11 21:49:31 +0000

    start porting to callables

--- functions/src/callable/tito.ts
+++ functions/src/callable/tito.ts
@@ -1,28 +1,91 @@
+import { CallableContext, HttpsError } from 'firebase-functions/lib/providers/https';
+import { auth } from 'firebase-admin';
 import { config } from 'firebase-functions';
-import { HttpsError } from 'firebase-functions/lib/providers/https';
 import { Ticket } from '../types/Ticket';
 import TitoService from '../services/tito';
+import { updateUser } from '../services/db';
 
 /**
  * Finds a single ticket by reference.
  *
- * @param reference Unique Ticket Reference to lookup
+ * @param data object containing two required parameters
+ * @param data.email Email associated with the ticket
+ * @param data.reference Unique Ticket Reference to lookup
  * @returns a Ticket
  * @throws if there is an error looking up the reference
  * @throws if the ticket reference is not unique
  */
-export const lookup = async (reference: string): Promise<Ticket> => {
+export const lookup = async ({ reference, email }: { email: string; reference: string }): Promise<Ticket> => {
   try {
     const titoInstance = new TitoService(config().tito.token, config().tito.organization, config().tito.event);
 
+    if (!reference || !email) {
+      throw new HttpsError('invalid-argument', 'Reference and email are required');
+    }
+
+    if (typeof reference !== 'string' || typeof email !== 'string') {
+      throw new HttpsError('invalid-argument', 'Reference and email must be strings');
+    }
+
     const ticket = await titoInstance.findTicket(reference);
 
-    if (ticket) {
+    if (ticket && ticket.email === email) {
       return ticket;
+    }
+
+    if (ticket) {
+      throw new HttpsError('permission-denied', 'Email or Reference do not match');
     }
 
     throw new HttpsError('not-found', 'Unique ticket not found.');
   } catch (e) {
+    if (e instanceof HttpsError) {
+      throw e;
+    }
+    console.error(e);
+    throw new HttpsError('internal', e.getMessage());
+  }
+};
+
+export const confirmTicket = async (slug: string, context: CallableContext): Promise<void> => {
+  try {
+    if (!context.auth) {
+      throw new HttpsError('unauthenticated', 'Not authenticated');
+    }
+
+    const { uid } = context.auth;
+
+    const titoInstance = new TitoService(config().tito.token, config().tito.organization, config().tito.event);
+
+    const ticket = await titoInstance.getTicket(slug);
+
+    await updateUser(uid, {
+      ticketReference: ticket.reference,
+      ticketSlug: slug,
+      phoneNumber: ticket.phoneNumber,
+      email: ticket.email,
+      firstName: ticket.first_name,
+      lastName: ticket.last_name,
+      fullName: ticket.name,
+      verified: false,
+    });
+
+    await auth().updateUser(uid, {
+      email: ticket.email,
+      displayName: ticket.name,
+      phoneNumber: ticket.phoneNumber,
+    });
+
+    return;
+  } catch (e) {
+    if (e instanceof HttpsError) {
+      throw e;
+    }
+
+    if (e.isAxiosError && e.response && e.response.status === 404) {
+      throw new HttpsError('not-found', 'Invalid ticket slug');
+    }
+
     console.error(e);
     throw new HttpsError('internal', e.getMessage());
   }
--- functions/src/routes/tito.ts
+++ functions/src/routes/tito.ts
@@ -1,77 +1,21 @@
-import { auth, firestore } from 'firebase-admin';
-import { config, Request, Response } from 'firebase-functions';
-import { createUser, updateUser } from '../services/db';
+import { CallableContext, HttpsError } from 'firebase-functions/lib/providers/https';
+import { confirmTicket, lookup } from '../callable/tito';
+import { Request, Response } from 'firebase-functions';
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { Ticket } from '../types/Ticket';
-import TitoService from '../services/tito';
-import { User } from '../types/User';
 
 const router = Router();
-const db = firestore();
-
-router.post('/webhook', async (req: Request, res: Response) => {
-  const body = req.body as Ticket;
-
-  try {
-    const docRef = db.doc(`tickets/${body.slug}`);
-    if ((await docRef.get()).exists) {
-      await docRef.set(body);
-      return res.status(StatusCodes.NO_CONTENT).send();
-    }
-
-    await docRef.set(body);
-    return res.status(StatusCodes.CREATED).send();
-  } catch (e) {
-    throw e;
-  }
-});
-
-router.get('/ticket/:reference', async (req: Request, res: Response) => {
-  try {
-    const titoInstance = new TitoService(config().tito.token, config().tito.organization, config().tito.event);
-    const { reference } = req.params;
-
-    const ticket = await titoInstance.findTicket(reference);
-
-    if (ticket) {
-      return res.json(ticket);
-    }
-
-    return res.status(StatusCodes.NOT_FOUND).json({ error: true, message: 'Unique ticket not found.' });
-  } catch (e) {
-    console.error(e);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: true, message: e.getMessage });
-  }
-});
 
 router.get('/ticket', async (req: Request, res: Response) => {
   try {
-    const titoInstance = new TitoService(config().tito.token, config().tito.organization, config().tito.event);
-    const { reference, email } = req.query;
+    const { email, reference } = req.query as { [key: string]: string };
+    const ticket = await lookup({ email, reference });
 
-    console.log(reference, email);
-
-    if (!reference || !email) {
-      return res.status(StatusCodes.BAD_REQUEST).send('Reference and email are required');
-    }
-
-    if (typeof reference !== 'string' || typeof email !== 'string') {
-      return res.status(StatusCodes.BAD_REQUEST).send('Reference and email must be strings');
-    }
-
-    const ticket = await titoInstance.findTicket(reference);
-
-    if (ticket && ticket.email === email) {
-      return res.json(ticket);
-    }
-
-    if (ticket) {
-      return res.status(StatusCodes.FORBIDDEN).json({ error: true, message: 'Email or Reference do not match' });
-    }
-
-    return res.status(StatusCodes.NOT_FOUND).json({ error: true, message: 'Unique ticket not found.' });
+    return res.json(ticket);
   } catch (e) {
+    if (e instanceof HttpsError) {
+      return res.status(e.httpErrorCode.status).json({ error: true, message: e.message });
+    }
     console.error(e);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: true, message: e.getMessage });
   }
@@ -83,69 +27,16 @@ router.put('/ticket/:slug/confirm', async (req: Request, res: Response) => {
       return res.status(StatusCodes.UNAUTHORIZED).send();
     }
 
-    const titoInstance = new TitoService(config().tito.token, config().tito.organization, config().tito.event);
-
+    const context = { auth: { uid: req.headers.authorization } } as CallableContext;
     const { slug } = req.params;
 
-    const ticket = await titoInstance.getTicket(slug);
-
-    await updateUser(req.headers.authorization, {
-      ticketReference: ticket.reference,
-      ticketSlug: slug,
-      phoneNumber: ticket.phoneNumber,
-      email: ticket.email,
-      firstName: ticket.first_name,
-      lastName: ticket.last_name,
-      fullName: ticket.name,
-      verified: false,
-    });
-
-    await auth().updateUser(req.headers.authorization, {
-      email: ticket.email,
-      displayName: ticket.name,
-      phoneNumber: ticket.phoneNumber,
-    });
+    await confirmTicket(slug, context);
 
     return res.status(StatusCodes.OK).send('updated');
   } catch (e) {
-    if (e.isAxiosError && e.response && e.response.status === 404) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: true, message: 'Invalid ticket slug' });
+    if (e instanceof HttpsError) {
+      return res.status(e.httpErrorCode.status).json({ error: true, message: e.message });
     }
-
-    console.error(e);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: true, message: e.getMessage });
-  }
-});
-
-router.post('/ticket/:reference/createUser', async (req: Request, res: Response) => {
-  try {
-    const titoInstance = new TitoService(config().tito.token, config().tito.organization, config().tito.event);
-    const { reference } = req.params;
-
-    const ticket = await titoInstance.findTicket(reference);
-
-    if (!ticket) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: true, message: 'Unique ticket not found.' });
-    }
-
-    const fullTicket = await titoInstance.getTicket(ticket.slug);
-
-    const newUser: User = {
-      uid: ticket.slug,
-      ticketSlug: ticket.slug,
-      ticketReference: reference,
-      phoneNumber: fullTicket.phoneNumber,
-      email: ticket.email,
-      firstName: ticket.first_name,
-      lastName: ticket.last_name,
-      fullName: ticket.name,
-      verified: false,
-    };
-
-    const user = await createUser(newUser);
-
-    return res.status(StatusCodes.CREATED).json(user);
-  } catch (e) {
     console.error(e);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: true, message: e.getMessage });
   }
