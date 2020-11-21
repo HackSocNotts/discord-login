e1e457e767bbfb2bed7a1d88501c029371bd2983
Aaron Osher <git@aaronosher.io>
2020-11-21 21:25:29 +0000

    Update Icon and Metadata

--- website/src/components/HeaderBar/HeaderBar.tsx
+++ website/src/components/HeaderBar/HeaderBar.tsx
@@ -3,10 +3,10 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import useStyles from "./HeaderBar.styles";
-import HackSocIcon from "../../Icons/HackSocIcon";
 import { Button } from "@material-ui/core";
 import { useTypedSelector } from "../../store";
 import { auth } from "../../firebase";
+import HackNottsIcon from "../../Icons/HackNottsIcon";
 
 const HeaderBar: React.FC = () => {
   const classes = useStyles();
@@ -17,7 +17,7 @@ const HeaderBar: React.FC = () => {
   return (
     <AppBar position="sticky" variant="outlined">
       <Toolbar>
-        <HackSocIcon className={classes.icon} color="inherit" />
+        <HackNottsIcon className={classes.icon} color="inherit" />
         <Typography
           variant="h6"
           color="inherit"
